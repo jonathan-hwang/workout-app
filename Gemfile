2a6aeb076a8da1cc51fbb63965034e7766cc93cc
Jonathan Hwang <jhwang05@gmail.com>
2014-01-27 22:03:06 -0500

    Add User Model

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'will_paginate', '3.0.4'
 gem 'bootstrap-will_paginate', '0.0.9'
 
 group :development, :test do
+  #gem 'mysql2'
   gem 'sqlite3', '1.3.8'
   gem 'rspec-rails', '2.13.1'
 end
---
+++ spec/views/users/edit.html.erb_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe "users/edit" do
+  before(:each) do
+    @user = assign(:user, stub_model(User,
+      :firstName => "MyString",
+      :lastName => "MyString",
+      :email => "MyString",
+      :age => "",
+      :gender => ""
+    ))
+  end
+
+  it "renders the edit user form" do
+    render
+
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "form[action=?][method=?]", user_path(@user), "post" do
+      assert_select "input#user_firstName[name=?]", "user[firstName]"
+      assert_select "input#user_lastName[name=?]", "user[lastName]"
+      assert_select "input#user_email[name=?]", "user[email]"
+      assert_select "input#user_age[name=?]", "user[age]"
+      assert_select "input#user_gender[name=?]", "user[gender]"
+    end
+  end
+end
