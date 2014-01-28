87b5755fde21029a41784162a8a37a423f7c7983
Jonathan Hwang <jhwang05@gmail.com>
2014-01-27 22:59:32 -0500

    has everything

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @user = User.new(firstName: "First", lastName: "Last", email: "example@email.com",
+                     age: 20, gender: 0) }
+                
+  subject { @user }
+  
+  it { should respond_to(:firstName) }
+  it { should respond_to(:lastName) }
+  it { should respond_to(:email) }
+  it { should respond_to(:age) }
+  it { should respond_to(:gender) }
 end
